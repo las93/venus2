342c09442a0bf6c0de7a32094a4fb776c9aac692
las93 <las92i@hotmail.fr>
2015-08-23 23:45:55 +0200

    New organisation -> with buncle + many fixed + begin the assets

---
+++ public/index.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * bootstrap of the demo
+ *
+ * @author    	Judicaël Paquet <judicael.paquet@gmail.com>
+ * @copyright 	Copyright (c) 2013-2014 PAQUET Judicaël FR Inc. (https://github.com/las93)
+ * @license   	https://github.com/las93/venus2/blob/master/LICENSE.md Tout droit réservé à PAQUET Judicaël
+ * @version   	Release: 1.0.0
+ * @filesource	https://github.com/las93/venus2
+ * @link      	https://github.com/las93
+ * @since     	1.0
+ */
+error_reporting(E_ALL);
+ini_set('display_error', 1);
+
+require 'conf/AutoLoad.php';
+
+\Venus\lib\Debug::activateDebug();
+
+$oRouter = new \Venus\core\Router();
+$oRouter->run();
